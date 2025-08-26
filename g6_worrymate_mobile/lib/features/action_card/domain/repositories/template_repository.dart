0ba432e65261e93328532093797e53da2c7790e1
tsitiii <ziontaa9@gmail.com>
2025-08-26 15:51:25 +0300

    commented it out the error

--- g6_worrymate_mobile/lib/features/action_card/domain/repositories/template_repository.dart
+++ g6_worrymate_mobile/lib/features/action_card/domain/repositories/template_repository.dart
@@ -1,9 +1,9 @@
-import 'package:dartz/dartz.dart';
-import '../../../../../../lib/features/core/errors/failure.dart';
-import '../../../../../../lib/features/core/params/params.dart';
-import '../entities/template_entitiy.dart';
+// import 'package:dartz/dartz.dart';
+// import '../../../../../../lib/features/core/errors/failure.dart';
+// import '../../../../../../lib/features/core/params/params.dart';
+// import '../entities/template_entitiy.dart';
 
-abstract class TemplateRepository {
-  Future<Either<Failure, TemplateEntity>> getTemplate(
-      {required TemplateParams params});
-}
+// abstract class TemplateRepository {
+//   Future<Either<Failure, TemplateEntity>> getTemplate(
+//       {required TemplateParams params});
+// }
