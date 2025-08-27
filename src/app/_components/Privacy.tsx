5beef3fe745a53e1357bd00edc855207107332f6
Izzat Engida <izzatengida7@gmail.com>
2025-08-27 13:41:01 +0300

    addition of journal page

---
+++ src/app/_components/Privacy.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Privacy() {
+  return (
+   <div className=' w-1/2 rounded-3xl bg-[white] text-[#132A4F] h-auto drop-shadow-white'>
+      <form className="space-y-6">
+    
+       
+      </form>
+    </div>
+  )
+}
+
+export default Privacy
