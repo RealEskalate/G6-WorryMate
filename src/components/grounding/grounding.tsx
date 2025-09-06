736dc8451496be2187a60192a1d87b264ffc690e
BemnetKebede <bemnet026@gmail.com>
2025-09-06 07:17:57 +0300

    edited action card

---
+++ src/components/grounding/grounding.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const grounding = () => {
+  return (
+    <div>
+        <div>5-4-3-2-1 Grounding Exersise</div>
+        
+
+
+    </div>
+  )
+}
+
+export default grounding
